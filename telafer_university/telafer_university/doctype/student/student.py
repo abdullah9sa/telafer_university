8a4db20283cad7fc1573f6d2153fea4e72062799
Abdullah <abdullahalsalih@gmail.com>
2025-01-08 14:31:17 +0300

    fix

--- telafer_university/telafer_university/doctype/student/student.py
+++ telafer_university/telafer_university/doctype/student/student.py
@@ -5,8 +5,7 @@ from frappe import _
 
 
 class Student(Document):
-    pass
+    def before_insert(self):
+        if frappe.get_all("Student", filters={"owner": self.owner}, limit_page_length=1):
+            frappe.throw(_("لا يمكن تسجيل طالب جديد بنفس المستخدم"))
 
-            
-            
-            
