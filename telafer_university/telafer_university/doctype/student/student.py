2f3c3daf5d3d23a2844f92d8bb57712d3edfb09b
Abdullah <abdullahalsalih@gmail.com>
2025-01-08 20:29:36 +0300

    pass

--- telafer_university/telafer_university/doctype/student/student.py
+++ telafer_university/telafer_university/doctype/student/student.py
@@ -4,6 +4,7 @@ from frappe.model.document import Document
 from frappe import _
 
 class Student(Document):
-    def before_save(self):
-        if frappe.db.exists("Student", {"owner": self.owner}):
-            frappe.throw(_("A Student record already exists for this user."))
+    pass
+    # def before_save(self):
+    #     if frappe.db.exists("Student", {"owner": self.owner}):
+    #         frappe.throw(_("A Student record already exists for this user."))
