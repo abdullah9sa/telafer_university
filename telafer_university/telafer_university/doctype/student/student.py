276f2b13fe39ad9296ce7d98768421602a14bb01
Abdullah <abdullahalsalih@gmail.com>
2025-01-16 19:00:07 +0300

    print removal

--- telafer_university/telafer_university/doctype/student/student.py
+++ telafer_university/telafer_university/doctype/student/student.py
@@ -5,8 +5,6 @@ from frappe import _
 
 class Student(Document):
     def before_insert(self):
-        # Check if the user has the "Student" role
-        print(frappe.get_roles(self.owner))
         if "Student" in frappe.get_roles(self.owner):
             # Check if a Student record already exists for this user
             if frappe.db.exists("Student", {"owner": self.owner}):
